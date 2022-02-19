06fb344b7a16588903045097742309b3b4bfea93
Patrick <prateekb1912@gmail.com>
2022-02-19 16:06:33 +0530

    Add User model class

--- base/models.py
+++ base/models.py
@@ -7,3 +7,10 @@ class Product(models.Model):
     price = models.DecimalField(max_digits = 6, decimal_places = 2)
     avaialable = models.BinaryField()
     last_update = models.DateTimeField(auto_now = True)
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20, unique = True)
+    phone = models.BigIntegerField(max_length=10)
+    birth_date = models.DateField(null = True)
