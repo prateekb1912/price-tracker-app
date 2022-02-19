0069a2b4d5264141369db1fa9fb3227af2c791d2
Patrick <prateekb1912@gmail.com>
2022-02-19 16:03:24 +0530

    Create the product model class in app

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 300, null=False)
+    # description = models.TextField(null = True)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    avaialable = models.BinaryField()
+    last_update = models.DateTimeField(auto_now = True)
