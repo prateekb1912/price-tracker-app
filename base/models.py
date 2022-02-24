0329a137b0edbc89d8e093fb2bbd331772cc553e
Patrick <prateekb1912@gmail.com>
2022-02-24 15:14:08 +0530

    Add the watchlist model

--- base/models.py
+++ base/models.py
@@ -14,3 +14,10 @@ class User(models.Model):
     email = models.CharField(max_length=20, unique = True)
     phone = models.BigIntegerField(max_length=10)
     birth_date = models.DateField(null = True)
+
+class WatchList(models.Model):
+    title = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_created = True)
+    last_updated = models.DateTimeField(auto_now = True)
+    products = models.ManyToManyField(Product, related_name='lists')
