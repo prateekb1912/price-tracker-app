d97f48f260761fb71fcc1ff8da34676e12a4c8c7
Patrick <prateekb1912@gmail.com>
2022-02-24 15:30:34 +0530

    Add slug field in product table

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length = 300, null=False)
+    slug = models.SlugField(null=True)
     # description = models.TextField(null = True)
     price = models.DecimalField(max_digits = 6, decimal_places = 2)
     avaialable = models.BinaryField()
@@ -12,8 +13,8 @@ class User(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.CharField(max_length=20, unique = True)
-    phone = models.BigIntegerField(max_length=10)
-    birth_date = models.DateField(null = True)
+    # phone = models.BigIntegerField()
+    # birth_date = models.DateField(null = True)
 
 class WatchList(models.Model):
     title = models.CharField(max_length=255)
